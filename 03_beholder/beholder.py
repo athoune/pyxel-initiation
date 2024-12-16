6aab192bd17dc5758f3592613c76c4986f257f20
Mathieu Lecarme <mathieu@garambrogne.net>
2024-12-16 12:17:13 +0100

    Beholder draft.

---
+++ 03_beholder/beholder.py
@@ -0,0 +1,78 @@
+import pyxel
+
+TRANSPARENT = 11
+
+LOADING = 0
+FIRING = 1
+MOVING = 2
+WAITING = 3
+
+
+class Sprite:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.speed: int
+        self.angle: int   # bottom, left, right, top
+        self._images = []
+
+    def move(self):
+        d = [(0, 1), (1, 0),(-1,0), (0, 1)][self.angle]
+        self.x += d[0] * self.speed
+        self.y += d[1] * self.speed
+
+    def image(self) -> tuple:
+        raise NotImplemntedError
+
+    def draw(self):
+        pyxel.blt(self.x, self.y, 0, *(self.image()))
+
+
+class Beholder(Sprite):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.angle = 1
+        self.speed = 2
+        self.state = WAITING
+        self._main_images = [(i * 16, 0, 16, 16, TRANSPARENT)
+                             for i in range(4)]
+        self._loading_images = [(i * 16, 16, 16, 16, TRANSPARENT)
+                                for i in range(4)]
+
+    def image(self):
+        if self.state == LOADING:
+            return self._loading_images[self.angle]
+        else:
+            return self._main_images[self.angle]
+
+
+class Hero(Sprite):
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.speed = 4
+        self.angle = 2
+        self._images = [(i * 16, 32, 16, 16, TRANSPARENT)
+                       for i in range(4)]
+
+    def image(self):
+        return self._images[self.angle]
+
+
+class App:
+    def __init__(self):
+        pyxel.init(160, 120, title="Flying bat")  # width, height, title
+        pyxel.load("beholder.pyxres")  # Load the assets
+        self.hero = Hero(32, 104)
+        self.beholder = Beholder(128, 32)
+        pyxel.run(self.update, self.draw)  # Starts Pyxel loop
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):  # Hit Q to quit
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)  # Clear screen
+        self.hero.draw()
+        self.beholder.draw()
+
+App()
