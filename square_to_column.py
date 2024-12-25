89e27bc487920923d4e976fe9b61ee8145477114
Mathieu Lecarme <mathieu@garambrogne.net>
2024-12-25 14:18:09 +0100

    fix: it's a function. Images source as arguments

--- square_to_column.py
+++ square_to_column.py
@@ -9,37 +9,48 @@ from PIL import Image
 
 SIZE = 16
 
-img = Image.open("bobby.png")
-print(img.size)
 
-width = int(img.size[0] / SIZE)
-height = int(img.size[1] / SIZE)
+def columnize(img: Image.Image) -> Image.Image:
+    width = int(img.size[0] / SIZE)
+    height = int(img.size[1] / SIZE)
 
-not_empty = 0
-for line in range(height):
-    for column in range(width):
-        first = img.getpixel((line, column))
-        ok = True
+    not_empty = 0  # Number of not emptye sprites
+    for line in range(height):
+        for column in range(width):
+            first = img.getpixel((line, column))
+            ok = True
 
-        for i in range(SIZE):
-            for j in range(SIZE):
-                p = img.getpixel((j + column * SIZE, i + line * SIZE))
-                if p != first:
-                    ok = False
-        if not ok:
-            not_empty = not_empty + 1
-        print(line, column, ok)
-print(not_empty)
+            for i in range(SIZE):
+                for j in range(SIZE):
+                    p = img.getpixel((j + column * SIZE, i + line * SIZE))
+                    if p != first:
+                        ok = False
+            if not ok:
+                not_empty = not_empty + 1
 
-frames = img.size[0] / SIZE * img.size[1] / SIZE
-out = Image.new("RGBA", (SIZE, not_empty * SIZE))
-# left, upper, right, lower
-i = 0
-for line in range(height):
-    for column in range(width):
-        thumb = img.crop(
-            (column * SIZE, line * SIZE, (column + 1) * SIZE, (line + 1) * SIZE)
-        )
-        out.paste(thumb, (0, i * SIZE))
-        i = i + 1
-out.save("out.png")
+    out = Image.new("RGBA", (SIZE, not_empty * SIZE))
+    i = 0
+    for line in range(height):
+        for column in range(width):
+            # left, upper, right, lower
+            thumb = img.crop(
+                (column * SIZE, line * SIZE, (column + 1) * SIZE, (line + 1) * SIZE)
+            )
+            out.paste(thumb, (0, i * SIZE))
+            i = i + 1
+    return out
+
+
+if __name__ == "__main__":
+    import sys
+    from pathlib import Path
+
+    if len(sys.argv) == 1:
+        print("I nedd some png paths as arguments")
+    else:
+        for square in sys.argv[1:]:
+            square = Path(square)
+            img = Image.open(square)
+            out = columnize(img)
+            out.save(square.stem + "_col" + square.suffixes[0])
+            print("✓", square)
