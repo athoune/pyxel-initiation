ddc32bdc0c28174b474ee2c1c45255b1e95e0691
Mathieu Lecarme <mathieu@garambrogne.net>
2025-01-02 15:04:28 +0100

    Rain draft.

---
+++ 05_rain/rain.py
@@ -0,0 +1,41 @@
+import pyxel
+
+TRANSPARENT = 0
+
+reeds = [(i * 8, 16, 8, 8, TRANSPARENT) for i in [4, 5, 6]]
+
+
+class App:
+    def __init__(self):
+        pyxel.init(80, 80, title="The rain")  # width, height, title
+        pyxel.images[1] = pyxel.Image.from_image("rain.png", incl_colors=True)
+        pyxel.tilemaps[1] = pyxel.Tilemap.from_tmx("rain.tmx", 0)
+        pyxel.tilemaps[1].imgsrc = 1  # The map use this image for its prites
+
+        pyxel.run(self.update, self.draw)  # Starts Pyxel loop
+
+    def update(self):
+        if pyxel.btnp(pyxel.KEY_Q):  # Hit Q to quit
+            pyxel.quit()
+
+    def draw(self):
+        pyxel.cls(0)  # Clear screen
+        # x, y, tm, u, v, w, h
+        pyxel.bltm(0, 0, 1, 0, 0, pyxel.width, pyxel.height - 24, TRANSPARENT)
+        for i in range(24):
+            delta = pyxel.frame_count % 24
+            pyxel.bltm(
+                i + delta * i // 24,  # x
+                pyxel.height - 24 + i,  # y
+                1,  # tm
+                0,  # u
+                pyxel.height - 24 + i,  # v
+                pyxel.width,  # w
+                1,  # h
+                TRANSPARENT,
+            )
+        for i in [1, 2, 7, 9]:
+            pyxel.blt(i * 8, 40, 1, *reeds[pyxel.frame_count // 10 % 3])
+
+
+App()
